f47447e83c89673d6d3b3291504a79d6a02a0ef5
burcu-erkan <burcuerkan.86@gmail.com>
2022-02-10 14:04:21 -0800

    fixed it

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -8,7 +8,9 @@ import millify from 'millify';
 import { baseUrl, fetchApi } from '../../utils/fetchApi';
 import ImageScrollbar from '../../components/ImageScrollbar';
 
+
 const PropertyDetails = ({ propertyDetails: { price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, furnishingStatus, amenities, photos } }) => (
+   
   <Box maxWidth='1000px' margin='auto' p='4'>
     {photos && <ImageScrollbar data={photos} />}
     <Box w='full' p='6'>
@@ -63,7 +65,7 @@ export default PropertyDetails;
 
 export async function getServerSideProps({ params: { id } }) {
   const data = await fetchApi(`${baseUrl}/properties/detail?externalID=${id}`);
-  
+
   return {
     props: {
       propertyDetails: data,
